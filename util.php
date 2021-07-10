376a33249ad63cd777d812147dbb3c94f681e29b
NasoohOlabi <nasooholabi@gmail.com>
2021-07-10 13:53:43 +0300

    util.php bug fix

--- util.php
+++ util.php
@@ -1,6 +1,8 @@
+<?php 
 function flashSessionAttribute ($str,$color = "green"){
 	if ( isset($_SESSION[$str]) ) {
 		echo("<p style=\"color:$color\">".$_SESSION[$str]."</p>\n");
 		unset($_SESSION[$str]);
 	}  
 }
+?>
